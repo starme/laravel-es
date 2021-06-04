80018aec7bc4c7801761cdf496483d68b450d7dd
starme <zj_ng@126.com>
2021-06-04 15:40:28 +0800

    init config

--- src/ElasticServiceProvider.php
+++ src/ElasticServiceProvider.php
@@ -14,6 +14,10 @@ class ElasticServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->publishes([
+            __DIR__ . '/config/es.php' => config_path('es.php'),
+        ], 'es-config');
+        
         Model::setConnectionResolver($this->app['es']);
 
 //        Model::setEventDispatcher($this->app['events']);
